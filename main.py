afb6a751b545db995338a657f34bf426a69bc4f0
aedX <timur.vdovin.08@gmail.com>
2025-04-12 14:45:51 +0700

    123

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, make_response, redirect, render_template, url_for, send_file
 from data import db_session, tables
-
-from werkzeug.security import generate_password_hash
+from flask_restful import reqparse, abort, Api, Resource
 
 import forms
 import stuff
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ZxrC#@wx-%08xKA9w-#ug2YB8c-A4IWoN#y'
+api = Api(app)
 
 
 @app.route('/')
@@ -30,12 +30,17 @@ def login():
 
 @app.route('/dashboard')
 def dashboard():
-    return send_file(stuff.create_qr('http://127.0.0.1:8080/dashboard'), mimetype='image/png')
+    db_sess = db_session.create_session()
+    username = request.cookies.get('user')
+    user = db_sess.query(tables.Worker).filter(tables.Worker.username == username).first()
+    title = f'{user.role} ({user.name})'
+    return render_template(f'{user.role}-dashboard.html', title=title)
+    # return send_file(stuff.create_qr('http://192.168.0.4:8080/dashboard'), mimetype='image/png')
 
 
 def main():
     db_session.global_init("db/data.db")
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8080, host='192.168.0.4')
 
 
 if __name__ == '__main__':
